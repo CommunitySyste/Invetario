b713234f1280f8c22199577befd4fff8581e0674
xavier <sanchezbyron988@gmail.com>
2026-05-19 22:00:19 -0500

    Sistema invetarios

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,8 @@ app.use(cors({
   origin: [
     'http://localhost',
     'http://localhost:5173',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://invetarioproyect.netlify.app/'
   ],
   credentials: true,
 }));
